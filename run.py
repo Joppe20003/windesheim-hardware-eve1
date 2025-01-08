1635795cbce67646926cc25c90ba18bf5e9a3b83
Joppe2003 <564285@edu.rocmn.nl>
2025-01-08 14:42:26 +0100

    com input var gemaakt

--- run.py
+++ run.py
@@ -3,7 +3,8 @@ from Modules.main import Main
 verwerkings_snelheid = int(input("Voer de verwerkings snelheid in: "))
 aankomst_snelheid_interval = int(input("Voer de aankomst snelheid interval in: "))
 capiciteit = int(input("Voer de capaciteit in: "))
+com = str(input("Voer de com poort voor de arduino: "))
 
-main = Main(aankomst_snelheid_interval, verwerkings_snelheid, capiciteit, 'COM3')
+main = Main(aankomst_snelheid_interval, verwerkings_snelheid, capiciteit, com)
 
 main.start()
